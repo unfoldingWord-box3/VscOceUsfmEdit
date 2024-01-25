78af2200cf81e910484fad3e22b880fe9018453a
blm <bruce.mclean@unfoldingword.org>
2024-01-25 05:29:58 -0500

    add watch for webview

--- webview-ui/src/AlignmentDialogWrapper.tsx
+++ webview-ui/src/AlignmentDialogWrapper.tsx
@@ -27,7 +27,7 @@ const AlignmentDialogWrapper: React.FC<AlignmentDialogWrapperProps> = ({
     const [smallFile, setSmallFile] = React.useState<string>("");
 
     async function getSourceMap(){
-        console.log( "requesting sourceMap.", reference );
+        console.log( "requesting sourceMap:", reference );
         let sourceMap : any = await getConfiguration("sourceMap");
         if( Object.keys(sourceMap).length == 0 ){ 
             sourceMap = {"(.*)\\.usfm": "source/\\1.usfm"}; 
@@ -36,14 +36,14 @@ const AlignmentDialogWrapper: React.FC<AlignmentDialogWrapperProps> = ({
         setSourceMap(sourceMap);
     }
     if( Object.keys(sourceMap).length == 0 ){ 
-        console.log( "requesting smallFile.", {reference} );
+        console.log( "requesting smallFile:", {reference} );
         getSourceMap(); 
     }else{
-        console.log( "Have sourceMap. " + sourceMap );
+        console.log( "Have sourceMap:", sourceMap );
     }
 
     async function getSmallFile(){
-        console.log( "requesting smallFile.", {reference} );
+        console.log( "requesting smallFile:", {reference} );
         let fileContent = await getFile("./bob.txt");
         console.log( "received smallFile. " + fileContent );
         if( fileContent ){
@@ -51,7 +51,7 @@ const AlignmentDialogWrapper: React.FC<AlignmentDialogWrapperProps> = ({
         }
     }
     if( smallFile === "" ){ 
-        console.log( "requesting smallFile.", {reference} );
+        console.log( "requesting smallFile:", {reference} );
         getSmallFile(); 
     } else {
         console.log( "Have smallFile. " + smallFile );
